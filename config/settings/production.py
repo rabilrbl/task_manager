5215cff68bc10138c3630224866f39ca0b4acecc
Mohammed Rabil <rabil@techie.com>
2022-02-18 00:51:23 +0530

    up

--- config/settings/production.py
+++ config/settings/production.py
@@ -108,7 +108,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join('staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
