d4df077200c0bbe1a0fdc67e2fa7aa990842d51b
Mohammed Rabil <rabil@techie.com>
2022-04-16 08:30:20 +0530

    Remove board filter

--- task_manager/tasks/api/views.py
+++ task_manager/tasks/api/views.py
@@ -42,7 +42,7 @@ class FilterClass(FilterSet):
     )
     status = ChoiceFilter(choices=STATUS_CHOICES)
     priority = ChoiceFilter(choices=PRIORITY_CHOICES)
-    board = ChoiceFilter(choices=Board.objects.filter(deleted=False).values_list("id", "title"))
+    # board = ChoiceFilter(choices=Board.objects.filter(deleted=False).values_list("id", "title"))
 
 
 class UserSerializer(BaseSerializer):
